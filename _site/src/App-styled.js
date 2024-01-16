21ae21e5b220d9ed6fd8f7c22757d755526fd11e
biancadizio <bianca.gdizio@gmail.com>
2024-01-16 20:55:10 -0300

    configuração deploy no package.json

--- _site/src/App-styled.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Container = styled.div`
-    width: 100%;
-    height: 100%;
-    background-color: red;
-    margin: 0;
-    // padding: 0 0 20px 0;
-    `;
--- _site/src/Globalstyles.js
+++
@@ -1,9 +0,0 @@
-// import { createGlobalStyle } from 'styled-components';
-
-// export const GlobalStyle = createGlobalStyle`
-//   body {
-//     font-family: 'Arial', sans-serif;
-//     background-color: #F4F4F4;
-//     color: #333;
-//   }
-// `;
